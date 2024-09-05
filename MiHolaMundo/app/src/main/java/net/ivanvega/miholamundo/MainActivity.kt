64d4f8294683915a96b06f7fad10b94c52347faa
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-09-05 15:59:53 -0600

    Se agrego Annimaciones y se comenzo a modificar el theme

--- MiHolaMundo/app/src/main/java/net/ivanvega/miholamundo/MainActivity.kt
+++ MiHolaMundo/app/src/main/java/net/ivanvega/miholamundo/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,13 +24,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import net.ivanvega.miholamundo.ui.theme.MiHolaMundoTheme
 
@@ -83,11 +90,21 @@ fun Greetings(modifier: Modifier,
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     //var expanded = false
-    val expanded = remember {
+    /*val expanded = remember {
+        mutableStateOf(false)
+    }*/
+    var expanded by rememberSaveable {
         mutableStateOf(false)
     }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-
+    val extraPadding by animateDpAsState(
+                            if (expanded) 48.dp else 0.dp,
+                            animationSpec = spring(
+                                dampingRatio = Spring.DampingRatioMediumBouncy,
+                                stiffness = Spring.StiffnessLow
+                            )
+                            //tween
+                            //repeatable
+                        )
     Surface (color = MaterialTheme.colorScheme.primary
         , modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
@@ -97,21 +114,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 modifier =
                 Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
 
             ) {
                 Text(text = "Hello ",
                     )
                 Text(text = "$name!",
+                        style = MaterialTheme.typography.headlineMedium.copy(
+                            fontWeight = FontWeight.ExtraBold
+                        )
                     )
             }
             ElevatedButton(
                 onClick = {
-                    expanded.value = !expanded.value
+                    expanded = !expanded
                 }
             )
             {
-                Text(text = if (expanded.value) "Show less" else "Show more"  )
+                Text(text = if (expanded) "Show less" else "Show more"  )
             }
         }
 
