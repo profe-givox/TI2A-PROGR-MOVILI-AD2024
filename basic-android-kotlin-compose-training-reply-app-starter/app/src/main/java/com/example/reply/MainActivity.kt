5c24192df7efc1bbad2f95f55b6079874d9e5333
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-10-14 16:06:34 -0600

    Se adiciono la Api androidx.compose.material3:material3-window-size-class
    Se comenzo a validar elmyamaño de la pantalla para cargar el tipo de navegación

--- basic-android-kotlin-compose-training-reply-app-starter/app/src/main/java/com/example/reply/MainActivity.kt
+++ basic-android-kotlin-compose-training-reply-app-starter/app/src/main/java/com/example/reply/MainActivity.kt
@@ -26,6 +26,9 @@ import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
@@ -35,12 +38,14 @@ import com.example.reply.ui.theme.ReplyTheme
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             ReplyTheme {
                 val layoutDirection = LocalLayoutDirection.current
+                val windowSize = calculateWindowSizeClass(this)
                 Surface(
                     modifier = Modifier
                         .padding(
@@ -50,7 +55,7 @@ class MainActivity : ComponentActivity() {
                                 .calculateEndPadding(layoutDirection)
                         )
                 ) {
-                    ReplyApp()
+                    ReplyApp(windowSize = windowSize.widthSizeClass)
                 }
             }
         }
@@ -62,7 +67,7 @@ class MainActivity : ComponentActivity() {
 fun ReplyAppCompactPreview() {
     ReplyTheme {
         Surface {
-            ReplyApp()
+            ReplyApp(windowSize = WindowWidthSizeClass.Compact)
         }
     }
 }
